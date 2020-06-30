9e02b02edac5658fa06be1ed034557565993dde0
Andreff16 <andr.b.2001@gmail.com>
2020-06-30 19:42:19 +0300

    Graph generator

--- js/main.js
+++ js/main.js
@@ -1,10 +1,1 @@
-function include(url) {
-	let script;
-
-	script = document.createElement("script");
-	script.src = url;
-	document.getElementsByTagName("body")[0].insertBefore(script, document.getElementById("main_script"));
-}
-
-include("js/draw_graph.js");
 draw_graph(10);
