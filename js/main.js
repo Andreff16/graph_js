54646e67d6f2bda2309f050db16c3738cb7a296d
Andreff16 <andr.b.2001@gmail.com>
2020-07-04 22:20:45 +0300

    plus lines

--- js/main.js
+++ js/main.js
@@ -7,12 +7,3 @@ drawGraphButton.onclick = function()
 	clear_graph();
 	mx = draw_graph(get_nodes_nbr());
 };
-let node1 = {x: 45, y: 45};
-let node2 = {x: 345, y: 195};
-let node3 = {x: 450, y: 450};
-
-draw_node(node1);
-draw_node(node2);
-draw_node(node3);
-draw_line(node1, node2);
-draw_line(node2, node3);
