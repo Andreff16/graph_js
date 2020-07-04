3f3b640fbe50f66bcfb01caaeff60a6a90a1c930
Andreff16 <andr.b.2001@gmail.com>
2020-07-04 22:18:45 +0300

    +linesgit status
    qw
    q

--- js/draw_graph.js
+++ js/draw_graph.js
@@ -1,42 +1,160 @@
-function create_node(map, x, y)
+function mx_show(mx)
 {
-	node = document.createElement("span");
-	node.className = "node";
-	node.style.left = x + "px";
-	node.style.bottom = y + "px";
-	map.appendChild(node);
+	let i;
+
+	i = 0;
+	while (i < mx.length)
+	{
+		console.log(mx[i]);
+		i++;
+	}
+}
+
+function mx_fill(node_count)
+{
+	let mx;
+	let i;
+	let j;
+
+	mx = new Array(node_count);
+	i = 0;
+	while (i < node_count)
+	{
+		mx[i] = new Array(node_count);
+		j = 0;
+		while (j < node_count)
+		{
+			mx[i][j] = Math.floor(Math.random() * 10) % 2;
+			j++;
+		}
+		i++;
+	}
+	// mx_show(mx);
+	return (mx);
+}
+
+function draw_node(node)
+{
+	let html_node;
+
+	html_node = document.createElement("span");
+	html_node.className = "node";
+	html_node.style.left = node.x + "px";
+	html_node.style.bottom = node.y + "px";
+	document.getElementById("map").appendChild(html_node);
+}
+
+function get_line_width(node1, node2)
+{
+	let a;
+	let b;
+
+	a = Math.pow(Math.abs(node1.x - node2.x), 2);
+	b = Math.pow(Math.abs(node1.y - node2.y), 2);
+	return (Math.sqrt(a + b));
+}
+
+function get_line_deg(node1, node2)
+{
+	let rad;
+
+	rad = Math.atan((node2.y - node1.y) / (node1.x - node2.x))
+	return ((rad * 180) / Math.PI);
+}
+
+function get_line_x(node1, node2)
+{
+	let w;
+	let c;
+	let g;
+
+	w = get_line_width(node1, node2);
+	c = Math.cos(get_line_deg(node1, node2) * Math.PI / 180);
+	g = (w / 2) * c;
+	return (Math.min(node1.x, node2.x) - ((w / 2) - g) + 5);
+}
+
+function draw_line(node1, node2)
+{
+	let line;
+
+	line = document.createElement("hr");
+	line.className = "map_road";
+	line.style.width = get_line_width(node1, node2) + "px";
+	line.style.left = get_line_x(node1, node2) + "px";
+	line.style.bottom = (node1.y + 5 - (node1.y - node2.y) / 2) + "px";
+	line.style.transform = "rotate(" + get_line_deg(node1, node2) + "deg)";
+	document.getElementById("map").appendChild(line);
 }
 
 function draw_graph(node_count)
 {
-	let node;
-	let map;
 	let i;
+	let j;
+	let nodes;
+	let mx;
 
-	map = document.getElementById("map");
+	mx = mx_fill(node_count);
+	nodes = new Array(node_count);
 	i = 0;
 	while (i < node_count)
 	{
-		let x;
-		let y;
-
-		x = Math.floor(Math.random() * 450) + 10;
-		y = Math.floor(Math.random() * 450) + 10;
-		create_node(map, x, y);
+		nodes[i] = {
+			x: Math.floor(Math.random() * 10) * 50 + 45,
+			y: Math.floor(Math.random() * 10) * 50 + 45
+		}
+		draw_node(nodes[i]);
 		i++;
 	}
+	i = 0;
+	while (i < node_count)
+	{
+		j = i + 1;
+		while (j < node_count)
+		{
+			if (mx[i][j] > 0)
+				draw_line(nodes[i], nodes[j]);
+			j++;
+		}
+		i++;
+	}
+	return (mx);
 }
 
 function clear_graph()
 {
 	let nodes;
+	let roads;
 	let i;
+	let l;
 
 	nodes = document.getElementsByClassName("node");
+	roads = document.getElementsByClassName("map_road");
+	l = nodes.length;
 	i = 0;
-	while (i < nodes.length)
+	while (i < l)
 	{
-		nodes[i].remove();
+		nodes[0].remove();
 		i++;
 	}
+	l = roads.length;
+	i = 0;
+	while (i < l)
+	{
+		roads[0].remove();
+		i++;
+	}
+}
+
+function get_nodes_nbr()
+{
+	let nbr;
+
+	nbr = document.getElementById("nodes_nbr").value;
+	if (nbr >= 100)
+		return (100);
+	else if (nbr > 0)
+		return (nbr);
+	else
+		return (10);
 }
--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
-let drawGraphButton
+let drawGraphButton;
+let mx;
 
-drawGraphButton = document.getElementById("draw_graph");
+drawGraphButton = document.getElementById("draw_graph_btn");
 drawGraphButton.onclick = function()
 {
-	draw_graph(10);
 	clear_graph();
+	mx = draw_graph(get_nodes_nbr());
 };
+let node1 = {x: 45, y: 45};
+let node2 = {x: 345, y: 195};
+let node3 = {x: 450, y: 450};
+
+draw_node(node1);
+draw_node(node2);
+draw_node(node3);
+draw_line(node1, node2);
+draw_line(node2, node3);
